2596810b811cc021f2bee3bf6bd9c56d2398f657
gavindang2911 <hoaidang2999@gmail.com>
2020-07-22 13:58:55 -0700

    Added button to finish the stopwatch

--- Stop_watch/app/src/main/java/app/Stopwatch/StopWatchActivity.java
+++ Stop_watch/app/src/main/java/app/Stopwatch/StopWatchActivity.java
@@ -2,14 +2,39 @@ package app.Stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class StopWatchActivity extends AppCompatActivity {
 
+    Button btnStart;
+    ImageView imgAnchor;
+    Animation rotate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
-        
+
+        btnStart = findViewById(R.id.btn_start);
+        imgAnchor = findViewById(R.id.imageAnchor);
+
+        rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
+
+        btnStart.setTypeface(MMedium);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgAnchor.startAnimation(rotate);
+            }
+        });
+
     }
 }
