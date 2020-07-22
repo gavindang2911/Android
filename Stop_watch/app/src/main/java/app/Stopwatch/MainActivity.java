ab863732d12341e064d2c7a371a93050ac696521
gavindang2911 <hoaidang2999@gmail.com>
2020-07-22 01:28:47 -0700

    Added font to the textView

--- Stop_watch/app/src/main/java/app/Stopwatch/MainActivity.java
+++ Stop_watch/app/src/main/java/app/Stopwatch/MainActivity.java
@@ -2,7 +2,10 @@ package app.Stopwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView titleWelcomeText = findViewById(R.id.tvSplash);
+        TextView bodyWelcomeText = findViewById(R.id.tvSplash);
+        Button btnStart = findViewById(R.id.btn_get_started);
+
+        Typeface MLight = Typeface.createFromAsset(getAssets(), "fonts/MLight.tff");
+        Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.tff");
+        Typeface MRegular = Typeface.createFromAsset(getAssets(), "fonts/MRegular.tff");
+
+        titleWelcomeText.setTypeface(MRegular);
+        bodyWelcomeText.setTypeface(MLight);
+        btnStart.setTypeface(MMedium);
+
     }
 }
