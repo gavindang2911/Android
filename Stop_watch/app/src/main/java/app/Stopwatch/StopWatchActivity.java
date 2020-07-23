078ae703619ecf00e4b4ee89fa5675dea7969e28
gavindang2911 <hoaidang2999@gmail.com>
2020-07-23 15:04:39 -0700

    Finishing the stop watch by adding Chronometer and function counting time

--- Stop_watch/app/src/main/java/app/Stopwatch/StopWatchActivity.java
+++ Stop_watch/app/src/main/java/app/Stopwatch/StopWatchActivity.java
@@ -4,35 +4,60 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.ImageView;
 
 public class StopWatchActivity extends AppCompatActivity {
 
     Button btnStart;
+    Button btnStop;
     ImageView imgAnchor;
     Animation rotate;
+    Chronometer time;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
 
         btnStart = findViewById(R.id.btn_start);
+        btnStop = findViewById(R.id.btn_finish);
         imgAnchor = findViewById(R.id.imageAnchor);
+        time = findViewById(R.id.timerHere);
+
+        btnStop.setAlpha(0);
 
         rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
 
         Typeface MMedium = Typeface.createFromAsset(getAssets(), "fonts/MMedium.ttf");
 
         btnStart.setTypeface(MMedium);
+        btnStop.setTypeface(MMedium);
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 imgAnchor.startAnimation(rotate);
+                btnStop.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnStart.animate().alpha(0).setDuration(300).start();
+                // Start time
+                time.setBase(SystemClock.elapsedRealtime());
+                time.start();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgAnchor.clearAnimation();
+                btnStart.animate().alpha(1).translationY(-80).setDuration(300).start();
+                btnStop.animate().alpha(0).setDuration(300).start();
+                time.setBase(SystemClock.elapsedRealtime());
+                time.stop();
             }
         });
 
